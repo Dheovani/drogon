47cd0bbd4d99c28be52aea766e5a7b56a7e779ce
Dheovani Xavier <79609196+Dheovani@users.noreply.github.com>
2023-02-26 19:18:36 -0300

    first commit

---
+++ controllers/ControllerTest.cc
@@ -0,0 +1,11 @@
+#include "ControllerTest.h"
+
+void ControllerTest::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+    auto resp = HttpResponse::newHttpResponse();
+    resp->setStatusCode(k200OK);
+    resp->setContentTypeCode(CT_TEXT_HTML);
+    resp->setBody("<h2>Testando o drogon</h2>");
+    callback(resp);
+}
