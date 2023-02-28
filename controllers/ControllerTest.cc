f5f4afc05f41729b723a3f0b372856c2f9eb3b7d
Dheovani Xavier <79609196+Dheovani@users.noreply.github.com>
2023-02-27 22:20:12 -0300

    Criando mapper

--- controllers/ControllerTest.cc
+++ controllers/ControllerTest.cc
@@ -1,11 +1,37 @@
 #include "ControllerTest.h"
+#include <DrogonTest.h>
 
-void ControllerTest::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
-{
-    // write your application logic here
-    auto resp = HttpResponse::newHttpResponse();
+using namespace drogon_model::postgres;
+
+void ControllerTest::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback) {
+    Json::Value response, columns;
+
+    std::string date = std::format("{:%Y-%m-%d %X}", std::chrono::system_clock::now());
+    columns["msg_date"] = date;
+    columns["message"] = req->body().data();
+    columns["errors"] = NULL;
+    
+    drogon::orm::Mapper<DrogonTest> mapper(drogon::app().getDbClient());
+    DrogonTest drogonTest(columns);
+    try {
+        mapper.insert(drogonTest);
+
+        response["success"] = TRUE;
+        response["message"] = "Succesfully isnerted into database";
+        response["dbContent"] = "result";
+    }
+    catch (const std::exception& ex) {
+        columns["errors"] = ex.what();
+        mapper.insert(drogonTest);
+
+        // Tratamento de error não devolve os dados do banco
+        response["success"] = FALSE;
+        response["message"] = ex.what();
+    }
+
+    HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(response);
     resp->setStatusCode(k200OK);
-    resp->setContentTypeCode(CT_TEXT_HTML);
-    resp->setBody("<h2>Testando o drogon</h2>");
+    resp->setContentTypeCode(CT_APPLICATION_JSON);
+
     callback(resp);
 }
